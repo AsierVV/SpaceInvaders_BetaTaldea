704670e3530604e8ee61f3d05963ab99f9b1d7d3
marco <marco@localhost>
2026-02-24 17:48:51 +0100

    Tableroaren atributuak eta eraikitzailea.

--- SpaceInvaders_BetaTaldea/src/Tableroa.java
+++ SpaceInvaders_BetaTaldea/src/Tableroa.java
@@ -1,4 +1,18 @@
 
 public class Tableroa {
+	private Pixel[][] tableroMatrizea;
+	
+	public Tableroa(int errenkadak, int zutabeak) {
+        tableroMatrizea = new Pixel[errenkadak][zutabeak];
 
+        for (int i = 0; i < errenkadak; i++) {
+            for (int j = 0; j < zutabeak; j++) {
+                tableroMatrizea[i][j] = new Pixel();
+            }
+        }
+    }
+	
+	 public Pixel getPixel(int errenkadak, int zutabeak) {
+	        return tableroMatrizea[errenkadak][zutabeak];
+	    }
 }
