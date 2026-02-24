0a99562b069521fbb4d4c3bf66af8598434cff93
AsierVV <avaldelviravazquez@gmail.com>
2026-02-24 17:57:52 +0100

    Hegazkina clasea

--- SpaceInvaders_BetaTaldea/src/modeloa/Hegazkina.java
+++ SpaceInvaders_BetaTaldea/src/modeloa/Hegazkina.java
@@ -2,7 +2,40 @@ package modeloa;
 
 public class Hegazkina extends Pixel{
 
-	
+    private int max_X = 99;
+    private int max_Y = 59;
+
+    public Hegazkina(Koordenatua k) {
+    	super(k);
+    }
+
+    public Koordenatua getPosizioa() {
+        return pos;
+    }
+
+    public void mugituGora() {
+        if (pos.getKordY() > 0) {
+            pos.setY(pos.getKordY() - 1);
+        }
+    }
+
+    public void mugituBehera() {
+        if (pos.getKordY() < max_Y) {
+            pos.setY(pos.getKordY() + 1);
+        }
+    }
+
+    public void mugituEzkerra() {
+        if (pos.getKordX() > 0) {
+            pos.setX(pos.getKordX() - 1);
+        }
+    }
+
+    public void mugituEskuina() {
+        if (pos.getKordX() < max_X) {
+            pos.setX(pos.getKordX() + 1);
+        }
+    }
 
 	@Override
 	public void koordenatuakEguneratu() {}
