0efcf6c55c742aab9caadeed857709262228265c
anderRaya <anderraya10@gmail.com>
2026-02-25 10:40:09 +0100

    a

--- SpaceInvaders_BetaTaldea/src/modeloa/Etsaia.java
+++ SpaceInvaders_BetaTaldea/src/modeloa/Etsaia.java
@@ -1,7 +1,28 @@
 package modeloa;
 
 public class Etsaia extends Pixel{
-
+	private boolean bizirik;
+	private int mugimenduAbiadura;
+	
+	public Etsaia(Koordenatua k) {
+		super(k);
+		this.bizirik = true;
+		this.mugimenduAbiadura = 200;
+	}
+    public boolean isBizirik() {
+        return bizirik;
+    }
+    
+    public void setBizirik(boolean bizirik) {
+        this.bizirik = bizirik;
+    }
+    public int getMugimenduAbiadura() {
+        return mugimenduAbiadura;
+    }
+    
+    public void setMugimenduAbiadura(int mugimenduAbiadura) {
+        this.mugimenduAbiadura = mugimenduAbiadura;
+    }
 	
 	@Override
 	public void koordenatuakEguneratu() {}
