f49b7ea2822448e954d22545d5dbea0c7ac4aae8
AsierVV <avaldelviravazquez@gmail.com>
2026-02-26 21:56:57 +0100

    Cambios en las clases tableroa y gelaxka. He añadido mas metodos y cambiado alguna cosa del eraikitzaile.

--- SpaceInvaders_BetaTaldea/src/modeloa/Gelaxka.java
+++ SpaceInvaders_BetaTaldea/src/modeloa/Gelaxka.java
@@ -1,21 +1,36 @@
 package modeloa;
 
 public class Gelaxka {
+	
 	private Koordenatua gelaxkaPosizioa;
 	private Pixel mota;
+	
+	/*
 	private int indize; //0 --> hutsunea
 						//1 --> hegazkina
 						//2-10 --> etsaia
 						//-1 --> tiroa
+	*/
 	
 	public Gelaxka(Koordenatua pKoord) {
 		this.gelaxkaPosizioa = pKoord;
 		this.mota = new Hutsunea(pKoord);
-		this.indize = -1;
+		//this.indize = pIndize;
 	}
 	
 	public void setMota(Pixel pMota) {
 		this.mota = pMota;
-		this.indize = 1;
+	}
+	
+	public Pixel getMota() {
+		return mota;
+	}
+	
+	public void hutsikUtzi() {
+	    this.mota = new Hutsunea(gelaxkaPosizioa);
+	}
+	
+	public Koordenatua getGelaxkaKord() {
+		return gelaxkaPosizioa;
 	}
 }
--- SpaceInvaders_BetaTaldea/src/modeloa/Tableroa.java
+++ SpaceInvaders_BetaTaldea/src/modeloa/Tableroa.java
@@ -1,25 +1,69 @@
 package modeloa;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Tableroa {
 	private Gelaxka[][] tableroMatrizea;
+	private Hegazkina hegazkina;
+	private List<Etsaia> etsaiak;
+	private List<Tiroa> tiroak;
+	
+	private final int zabalera = 100;
+    private final int altuera = 60;
 	
 	public Tableroa() {
-        tableroMatrizea = new Gelaxka[100][60];
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 60; j++) {
-            	Koordenatua k = new Koordenatua(i,j);
-                Gelaxka g = new Gelaxka(k);
-            	tableroMatrizea[i][j] = g;
+		
+        tableroMatrizea = new Gelaxka[zabalera][altuera];
+        etsaiak = new ArrayList<>();
+        tiroak = new ArrayList<>();
+        
+        for (int i = 0; i < zabalera; i++) {
+            for (int j = 0; j < altuera; j++) {
+            	tableroMatrizea[i][j] = new Gelaxka(new Koordenatua(i,j));
+            }
+        }
+        
+        sortuHegazkina();
+        sortuEtsaiak();
+    }
+	
+	private void sortuHegazkina() {
+        hegazkina = new Hegazkina();
+        tableroMatrizea[50][55].setMota(hegazkina);
+    }
+
+    private void sortuEtsaiak() {
+        Random r = new Random();
+        int kopurua = 4 + r.nextInt(5); // 4-8 etsai
+
+        while (etsaiak.size() < kopurua) {
+            int zutabea = r.nextInt(zabalera);
+
+            // Konprobatu ea hutsik dagoen gelaxka
+            if (tableroMatrizea[zutabea][5].getMota() instanceof Hutsunea) {
+                Etsaia e = new Etsaia(new Koordenatua(zutabea, 5));
+                etsaiak.add(e);
+                tableroMatrizea[zutabea][5].setMota(e);
             }
         }
     }
 	
-	 public Gelaxka getPixel(int errenkadak, int zutabeak) {
-	        return tableroMatrizea[errenkadak][zutabeak];
+	 public Gelaxka getGelaxka(int x, int y) {
+		 return tableroMatrizea[x][y];
 	 }
 	 
-	 public void setHegazkina() {
-		 Hegazkina h = new Hegazkina();
-		 tableroMatrizea[50][55].setMota(h); 
+	 public Hegazkina getHegazkina() {
+		 return hegazkina;
 	 }
+	 
+	 public List<Etsaia> getEtsaiak() {
+		 return etsaiak;
+	 }
+	 
+	 public List<Tiroa> getTiroak() {
+		 return tiroak;
+	 }
+
 }
