9aeae7b8666281fcede2706f817458f0f657d15e
anderRaya <anderraya10@gmail.com>
2026-02-28 18:38:58 +0100

    JokoPanela es la clase que dibuja en pantalla el estado actual del juego leyendo la información del modelo, sin modificar nada.

--- SpaceInvaders_BetaTaldea/src/game/main.java
+++ SpaceInvaders_BetaTaldea/src/game/main.java
@@ -1,9 +1,23 @@
 package game;
+
+import javax.swing.JFrame;
 import modeloa.Tableroa;
+import visual.JokoPanela;
 
 public class main {
+
     public static void main(String[] args) {
-        new Tableroa();
-        System.out.println("Programa iniciado");
+
+        Tableroa tableroa = new Tableroa();
+
+        JFrame frame = new JFrame("Space Invaders");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JokoPanela panel = new JokoPanela(tableroa);
+        frame.add(panel);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
---
+++ SpaceInvaders_BetaTaldea/src/visual/JokoPanela.java
@@ -0,0 +1,64 @@
+package visual;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.util.Observable;
+import java.util.Observer;
+
+import modeloa.Tableroa;
+import modeloa.Pixel;
+import modeloa.Hutsunea;
+import modeloa.Hegazkina;
+import modeloa.Etsaia;
+import modeloa.Tiroa;
+
+public class JokoPanela extends JPanel implements Observer {
+
+    private Tableroa model;
+    private final int TAM_CELDA = 10;
+
+    public JokoPanela(Tableroa model) {
+        this.model = model;
+        this.model.addObserver(this);
+
+        setPreferredSize(new Dimension(100 * TAM_CELDA, 60 * TAM_CELDA));
+        setBackground(Color.BLACK);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        for (int x = 0; x < 100; x++) {
+            for (int y = 0; y < 60; y++) {
+
+                Pixel p = model.getGelaxka(x, y).getMota();
+
+                if (p instanceof Hutsunea) {
+                    g.setColor(Color.BLACK);
+                } 
+                else if (p instanceof Hegazkina) {
+                    g.setColor(Color.GREEN);
+                } 
+                else if (p instanceof Etsaia) {
+                    g.setColor(Color.RED);
+                } 
+                else if (p instanceof Tiroa) {
+                    g.setColor(Color.YELLOW);
+                }
+
+                g.fillRect(x * TAM_CELDA,
+                           y * TAM_CELDA,
+                           TAM_CELDA,
+                           TAM_CELDA);
+            }
+        }
+    }
+}
