2cb894c34d346a3e0812584f1040d6eb6d64ead0
AsierVV <avaldelviravazquez@gmail.com>
2026-03-02 19:49:07 +0100

    Intento de HasierakoPantaila, esta bastante mal la verdad.

--- SpaceInvaders_BetaTaldea/src/visual/HasierakoPantaila.java
+++ SpaceInvaders_BetaTaldea/src/visual/HasierakoPantaila.java
@@ -1,5 +1,46 @@
 package visual;
 
-public class HasierakoPantaila {
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+public class HasierakoPantaila extends JPanel{
+	
+	private JPanel panel;
+	private JLabel HASIERA;
+	private JButton HASTEKO_BOTOIA;
+	public HasierakoPantaila() {
+		setLayout(new BorderLayout(0, 0));
+		add(getPanel(), BorderLayout.SOUTH);
+		add(getHASIERA(), BorderLayout.CENTER);
+	}
+	private JPanel getPanel() {
+		if (panel == null) {
+			panel = new JPanel();
+			panel.setLayout(new GridLayout(0, 1, 0, 0));
+			panel.add(getHASTEKO_BOTOIA());
+		}
+		return panel;
+	}
+	private JLabel getHASIERA() {
+		if (HASIERA == null) {
+			HASIERA = new JLabel("JOKO HASIERA");
+		}
+		return HASIERA;
+	}
+	private JButton getHASTEKO_BOTOIA() {
+		if (HASTEKO_BOTOIA == null) {
+			HASTEKO_BOTOIA = new JButton("HASI");
+			HASTEKO_BOTOIA.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					
+				}
+			});
+		}
+		return HASTEKO_BOTOIA;
+	}
 }
