5aa9cecbbe784aade0aedf918badf11f1eecbad6
anderRaya <anderraya10@gmail.com>
2026-03-03 19:34:08 +0100

    prueba

--- SpaceInvaders_BetaTaldea/src/visual/AmaierakoPantaila.java
+++ SpaceInvaders_BetaTaldea/src/visual/AmaierakoPantaila.java
@@ -3,18 +3,21 @@ package visual;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JTextField;
+import java.awt.CardLayout;
+import java.awt.FlowLayout;
 
 public class AmaierakoPantaila extends JPanel{
 	private JPanel panel;
 	private JTextField txtGalduDuzu;
 	public AmaierakoPantaila() {
 		setLayout(new BorderLayout(0, 0));
-		add(getPanel(), BorderLayout.CENTER);
+		add(getPanel());
 	}
 
 	private JPanel getPanel() {
 		if (panel == null) {
 			panel = new JPanel();
+			panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 			panel.add(getTxtGalduDuzu());
 		}
 		return panel;
