c7080181bf5c70f62715a66fc15b2c86eaa64185
anderRaya <anderraya10@gmail.com>
2026-03-06 12:48:14 +0100

    agregar boton para salir

--- SpaceInvaders_BetaTaldea/src/visual/IrabaziPantaila.java
+++ SpaceInvaders_BetaTaldea/src/visual/IrabaziPantaila.java
@@ -7,22 +7,34 @@ import java.awt.CardLayout;
 import java.awt.FlowLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
 
 public class IrabaziPantaila extends JPanel{
-	private JPanel panel; 
+	private JPanel panel;
+	private JPanel panel_1;
 	private JLabel lblNewLabel;
+	private JButton btnNewButton;
 	public IrabaziPantaila() {
 		setLayout(new BorderLayout(0, 0));
-		add(getPanel());
+		add(getPanel(), BorderLayout.CENTER);
+		add(getPanel_1(), BorderLayout.SOUTH);
 	}
-
 	private JPanel getPanel() {
 		if (panel == null) {
 			panel = new JPanel();
-			panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 			panel.add(getLblNewLabel());
 		}
 		return panel;
+	}
+	private JPanel getPanel_1() {
+		if (panel_1 == null) {
+			panel_1 = new JPanel();
+			panel_1.add(getBtnNewButton());
+		}
+		return panel_1;
 	}
 	private JLabel getLblNewLabel() {
 		if (lblNewLabel == null) {
@@ -30,5 +42,18 @@ public class IrabaziPantaila extends JPanel{
 			lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 28));
 		}
 		return lblNewLabel;
+	}
+	private JButton getBtnNewButton() {
+		if (btnNewButton == null) {
+			btnNewButton = new JButton("EXIT");
+	        btnNewButton.addActionListener(new ActionListener() {
+	            
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                System.exit(0);
+	            }
+	        });
+		}
+		return btnNewButton;
 	}
 }
