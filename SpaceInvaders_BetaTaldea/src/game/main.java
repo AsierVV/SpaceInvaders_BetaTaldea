3be45160604085177881be57f50895adbb7997cc
Marco <marcomateosgallego@gmail.com>
2026-03-10 17:56:59 +0100

    Mainaren aldaketa lehenak

--- SpaceInvaders_BetaTaldea/src/game/main.java
+++ SpaceInvaders_BetaTaldea/src/game/main.java
@@ -6,10 +6,15 @@ import javax.swing.SwingUtilities;
 import modeloa.Tableroa;
 import visual.JokoPanela;
 import visual.HasierakoPantaila;
-
+	
 public class main {
-
+	private static JokoKudeatzailea JK;
+	private static HasierakoPantaila HP;
+	
 	public static void main(String[] args) {
-	    new JokoKudeatzailea().abiarazi();
+	    JK = new JokoKudeatzailea();
+	    HP = new HasierakoPantaila();
 	}
 }
+
+//JK instantzia eta HP instantzia ere, JK dena erabakiko du
