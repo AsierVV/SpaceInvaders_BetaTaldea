62d00270cb8d7b683522504d94b7d00af4123ea9
anderRaya <anderraya10@gmail.com>
2026-03-24 18:28:46 +0100

    switch case

--- SpaceInvaders_BetaTaldea/src/modeloa/HegazkinaFactory.java
+++ SpaceInvaders_BetaTaldea/src/modeloa/HegazkinaFactory.java
@@ -2,4 +2,18 @@ package modeloa;
 
 public class HegazkinaFactory {
 
+	private static HegazkinaFactory nEMA = null;
+	
+	private HegazkinaFactory() {}
+	public static HegazkinaFactory nireEMA() {
+		if(nEMA == null) {nEMA = new HegazkinaFactory();}
+		return nEMA;
+	}
+	public Hegazkina sortuHegazkina(String mota, Koordenatua pPosizioa) {
+        switch (mota) {
+        case "GREEN": return new HegazkinaBerdea(pPosizioa);
+        case "BLUE": return new HegazkinUrdina(pPosizioa);
+        case "RED": return new HegazkinGorria(pPosizioa);
+        }
+	}
 }
