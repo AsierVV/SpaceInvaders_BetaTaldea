bcb221505297d1694222eb43d7731e85e01c2f7c
PapaJohns14 <ikermc35@gmail.com>
2026-03-31 18:28:41 +0200

    Tiro Motak

--- SpaceInvaders_BetaTaldea/src/modeloa/Hegazkina.java
+++ SpaceInvaders_BetaTaldea/src/modeloa/Hegazkina.java
@@ -1,8 +1,14 @@
 package modeloa;
 
 public class Hegazkina extends Pixel{
+	private TiroMota mota;
 	
 	protected Hegazkina(Koordenatua pPosizioa) {
 		super(pPosizioa);
+		this.mota= new TiroBakarra();
+	}
+	
+	public TiroMota getMota() {
+		return this.mota;
 	}
 }
---
+++ SpaceInvaders_BetaTaldea/src/modeloa/TiroRonbo.java
@@ -0,0 +1,9 @@
+package modeloa;
+
+public class TiroRonbo implements TiroMota {
+	@Override 
+	public void tiroEgin(int x, int y) {
+		
+	}
+
+}
