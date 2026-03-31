0d617215154a7adaf07aedb3b7cf67c662b4565d
anderRaya <anderraya10@gmail.com>
2026-03-31 18:36:41 +0200

    Etsia

--- SpaceInvaders_BetaTaldea/src/modeloa/EtsaiaFactory.java
+++ SpaceInvaders_BetaTaldea/src/modeloa/EtsaiaFactory.java
@@ -2,4 +2,21 @@ package modeloa;
 
 public class EtsaiaFactory {
 
+	private static EtsaiaFactory nEMA = null;
+	
+	private  EtsaiaFactory() {}
+	public static EtsaiaFactory nireEMA() {
+		if(nEMA == null) {nEMA = new EtsaiaFactory();}
+		return nEMA;
+	}
+	public static Etsaia sortuEtsaia(String mota, Koordenatua pPosizioa) {
+		Etsaia e = null;
+        switch (mota) {
+        case "BROWN": e= new EtsaiMarroia(pPosizioa); break;
+        case "PURPLE": e= new EtsaiMorea(pPosizioa); break;
+        case "RED": e= new EtsaiGorria(pPosizioa); break;
+        }
+        return e;
+	}
 }
+
